90303b2cb130cfe24dce7c959ff170125beaf077
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-02-16 14:24:05 -0600

    feat: Gameboard class can now place ships. Tests working.

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,22 +1,62 @@
-//Class tests
+// Ship tests
 import { Ship } from "./class.js";
+
 describe("Ship Class", () => {
   test("is created", () => {
-    expect(new Ship(4, 0)).toEqual({ length: 4, timesHit: 0, isSunk: false });
+    const ship = new Ship(4);
+    expect(ship.length).toBe(4);
+    expect(ship.timesHit).toBe(0);
+    expect(ship.isSunk).toBe(false);
   });
+
+  test("is hit", () => {
+    const ship = new Ship(4);
+    ship.hit();
+    expect(ship.timesHit).toBe(1);
+    expect(ship.isSunk).toBe(false);
+  });
+
   test("is sunk", () => {
     const ship = new Ship(4);
     ship.hit();
     ship.hit();
     ship.hit();
     ship.hit();
-    expect(ship).toEqual({ length: 4, timesHit: 4, isSunk: true });
+    expect(ship.isSunk).toBe(true);
   });
-  test("is hit", () => {
-    const ship = new Ship(4);
+});
 
-    ship.hit();
+// Gameboard tests
+import { Gameboard } from "./class.js";
 
-    expect(ship).toEqual({ length: 4, timesHit: 1, isSunk: false });
+describe("Gameboard Class", () => {
+  test("should be created with correct default values", () => {
+    const board = new Gameboard();
+
+    expect(board.grid.length).toBe(10);
+    board.grid.forEach((row) => expect(row.length).toBe(10));
+
+    expect(board.shipCoords).toEqual(new Map());
+    expect(board.misses).toEqual([]);
+  });
+
+  test("placeShip works", () => {
+    const board = new Gameboard();
+    board.placeShip(4, [3, 2], "vertical");
+
+    const shipKeys = Array.from(board.shipCoords.keys()); // Extract keys as an array
+
+    expect(shipKeys).toEqual(
+      expect.arrayContaining(["3,3", "3,4", "3,4", "3,5"])
+    );
+  });
+
+  test("receiveAttack hits ship", () => {
+    const board = new Gameboard();
+    board.placeShip(4, [3, 2], "vertical");
+
+    board.receiveAttack(3, 3);
+
+    expect(board.ships[0].timesHit).toBe(1);
   });
 });
--- src/class.js
+++ src/class.js
@@ -12,3 +12,31 @@ export class Ship {
     return this.timesHit;
   }
 }
+export class Gameboard {
+  constructor() {
+    this.grid = Array(10)
+      .fill(null)
+      .map(() => Array(10).fill(null));
+    this.shipCoords = new Map();
+    this.misses = [];
+  }
+  placeShip(length, startingPoint, direction) {
+    const ship = new Ship(length);
+    this.ships.push(ship);
+    let x = startingPoint[0];
+    let y = startingPoint[1];
+
+    for (let i = 0; i < length; i++) {
+      this.shipCoords.set(`${x},${y}`, ship);
+
+      if (direction === "horizontal") {
+        x++;
+      } else if (direction === "vertical") {
+        y++;
+      }
+    }
+  }
+
+  receiveAttack(x, y) {}
+  allSunk() {}
+}
