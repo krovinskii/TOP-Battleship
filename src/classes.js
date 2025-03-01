b8c398ff1f855b668656f6f383ec234c7ba8e6e8
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-03-01 11:47:55 -0600

    feat(gameboard): placeShip() working and tested

--- src/classes.js
+++ src/classes.js
@@ -1,3 +1,5 @@
+//All classes used in the game. Tests can be found in tests/classes.test.js
+
 export class Ship {
   constructor(length) {
     this.length = length;
@@ -17,6 +19,33 @@ export class Ship {
       return true;
     } else {
       return false;
+    }
+  }
+}
+export class Gameboard {
+  constructor() {
+    this.shipLocations = new Map();
+    this.misses = [];
+    this.allSunk = false;
+  }
+
+  placeShip(length, direction, coordXY) {
+    const ship = new Ship(length);
+    const x = coordXY[0];
+    const y = coordXY[1];
+
+    if (direction === "vertical") {
+      for (let i = 0; i < length; i++) {
+        const key = `${x},${y + i}`;
+        this.shipLocations.set(key, ship);
+      }
+    }
+
+    if (direction === "horizontal") {
+      for (let i = 0; i < length; i++) {
+        const key = `${x + i},${y}`;
+        this.shipLocations.set(key, ship);
+      }
     }
   }
 }
--- src/tests/classes.test.js
+++ src/tests/classes.test.js
@@ -1,5 +1,6 @@
 import { Ship } from "../classes";
-
+import { Gameboard } from "../classes";
+// || --------------------------Ship Class-----------------------//
 describe("Ship Class", () => {
   const dookieShip = new Ship(4);
   it("is Created", () => {
@@ -16,5 +17,23 @@ describe("Ship Class", () => {
     dookieShip.isHit();
     dookieShip.isHit();
     expect(dookieShip.sunk).toBe(true);
+  });
+});
+// || --------------------------Gameboard Class------------------//
+describe("Gameboard Class", () => {
+  const game = new Gameboard();
+  it("is created", () => {
+    expect(game.shipLocations.size).toBe(0);
+    expect(game.misses).toEqual([]);
+    expect(game.allSunk).toBe(false);
+  });
+  test("placeShip", () => {
+    game.placeShip(4, "vertical", [0, 0]);
+    expect([...game.shipLocations.entries()]).toEqual([
+      ["0,0", expect.objectContaining({ length: 4, timesHit: 0, sunk: false })],
+      ["0,1", expect.objectContaining({ length: 4, timesHit: 0, sunk: false })],
+      ["0,2", expect.objectContaining({ length: 4, timesHit: 0, sunk: false })],
+      ["0,3", expect.objectContaining({ length: 4, timesHit: 0, sunk: false })],
+    ]);
   });
 });
